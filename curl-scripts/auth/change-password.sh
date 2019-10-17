530996b95ccd545836276baa9c889988d5163d7b
GMorse19 <malorse@comcast.net>
2019-10-17 14:30:23 -0400

    Curl script for auth up and running
    Seems to be connecting to herokuapp
    
    Next I plan on writing event listeners

---
+++ curl-scripts/auth/change-password.sh
@@ -0,0 +1,13 @@
+curl "https://pacific-harbor-41390.herokuapp.com/change-password" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "passwords": {
+      "old": "'"${OLD}"'",
+      "new": "'"${NEW}"'"
+    }
+  }'
+
+echo
---
+++ curl-scripts/auth/sign-out.sh
@@ -0,0 +1,7 @@
+curl "https://pacific-harbor-41390.herokuapp.com/sign-out" \
+  --include \
+  --request DELETE \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}"
+
+echo
